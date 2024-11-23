81a66a887dd6ef29e3985889b27e37a8f0b3e448
Diego Sanchez <diegosmtattoo@gmail.com>
2024-11-23 18:52:18 -0300

    Implementação do metodo Instalar Aplicativo, herança das classes Nokia   e Iphone, teste das modificações.

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia:");
+Nokia nokia = new Nokia("11 123123123","modelo 1","123456781",512);
+nokia.Ligar();
+nokia.InstalarAplicativo("Duolingo");
+
+Console.WriteLine('\n');
+
+Console.WriteLine("Smartphone Iphone:");
+Iphone iphone = new Iphone("12 112233445","modelo 1","098765431",512);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("AppTeste");
